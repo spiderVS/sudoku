05fd229e6b1cc6ef55fa368cf41b3f808178ac01
spiderVS <fuzzy003@gmail.com>
2021-09-05 23:40:10 +0300

    init: start task, add findZero function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+
+  function findZero(matrix) {
+    for (let row = 0; row < matrix.length; row++) {
+      for (let col = 0; col < matrix[0].length; col++) {
+        if (matrix[row][col] === 0) return [row, col];
+      }
+    }
+    return null;
+  }
+
 }
