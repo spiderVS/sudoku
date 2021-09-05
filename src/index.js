ba528b8ddb96352fa258461f5081d98b5baef49d
spiderVS <fuzzy003@gmail.com>
2021-09-05 23:58:01 +0300

    feat: add checkNumber function

--- src/index.js
+++ src/index.js
@@ -9,4 +9,26 @@ module.exports = function solveSudoku(matrix) {
     return null;
   }
 
+  function checkNumber(number, position, matrix) {
+    let [row, col] = position;
+
+    for (let i = 0; i < matrix[row].length; i++) {
+      if ((matrix[row][i] == number) && (i != col)) return false;
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+      if ((matrix[i][col] == number) && (i != row)) return false;
+    }
+
+    let squareRow = Math.floor(row / 3) * 3;
+    let squareCol = Math.floor(col / 3) * 3;
+
+    for (let i = squareRow; i < squareRow + 3; i++) {
+      for (let j = squareCol; j < squareCol + 3; j++) {
+        if ((matrix[i][j] == num) && (i != row) && (i != col)) return false;
+      }
+    }
+  }
+
+
 }
